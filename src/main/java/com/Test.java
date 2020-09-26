30459964d4521e6123a210f8585e58586e3e8bce
fw1036994377@outlook.com <fw1036994377@outlook.com>
2020-09-26 22:35:02 +0800

    init

---
+++ src/main/java/com/Test.java
@@ -0,0 +1,12 @@
+package com;
+
+public class Test {
+
+
+
+    public void test(){
+
+        System.out.println("商鞅老师 -version 5.0");
+        System.out.println(this.getClass().getClassLoader());
+    }
+}
